a6589d302a362cb960a091e519019b9bcfca967f
artemiev <mixartemev@gmail.com>
2019-06-10 12:33:35 +0300

    buildings seed

---
+++ database/factories/BuildingFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory Factory */
+
+use App\Building;
+use App\MetroStation;
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
+
+$factory->define(Building::class, function (Faker $faker) {
+    return [
+        'address' => $faker->streetAddress,
+        'type' => $faker->randomElement([1,2,3,4]),
+        'profile' => $faker->randomElement([1,2,3]),
+        'metro_station_id' => $faker->randomElement([1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]),
+        'region_id' => $faker->randomElement([1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]),
+    ];
+});
