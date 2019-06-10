bb56208118d357a84ba45f1afd6b2ca4cbb152e4
artemiev <mixartemev@gmail.com>
2019-06-10 09:56:21 +0300

    models gen

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Reliese\Coders\CodersServiceProvider::class,
     ],
 
     /*
